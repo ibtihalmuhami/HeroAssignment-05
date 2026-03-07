1bcfaf7628fcf51b3381753644fc20e4fe555e82
ibtihalmuhami <ibtihalsamiha@gmail.com>
2026-03-07 06:44:36 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,25 @@
+console.log("Login script loaded");
+document.getElementById("login-btn").addEventListener("click", function() {
+    console.log("Login button clicked");
+    //1-- get the username input value
+    const inputUsername = document.getElementById("input-username");
+    const contactUsername = inputUsername.value;
+    console.log("Username:", contactUsername);
+
+    //2--get the password input value
+    const inputPassword = document.getElementById("input-password");
+    const contactPassword = inputPassword.value;
+    console.log("Password:", contactPassword);
+
+    //3-- match the username and password
+    if (contactUsername === "admin" && contactPassword === "admin123") {
+        alert("Login successful");
+    }
+    else {
+        alert("Login failed. Please check your username and password.");
+        return; // Exit the function if login fails
+    }
+
+
+
+});
